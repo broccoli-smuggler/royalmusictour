600acfd5ce75d8c75a07853dc460c34e4f72cd52
Joshua <joshua.jordan@reaktor.com>
2020-08-25 13:23:08 +0200

    minor cleanup

--- src/components/changingImageCanvas.jsx
+++ src/components/changingImageCanvas.jsx
@@ -14,7 +14,7 @@ const ChangingImageCanvas = ({image1, image2, percentage}) => {
     stage.addChild(after);
     stage.addChild(before);
 
-    composeBitmaps(percentage, stage, before.image.height, before.image.width);
+    composeBitmaps(percentage, before.image.height, before.image.width);
 
     after.image.onload = function() {
       canvas.width = after.image.width;
@@ -27,19 +27,17 @@ const ChangingImageCanvas = ({image1, image2, percentage}) => {
     };
   });
 
-  const composeBitmaps = (percentage, stage, height, width) => {
+  const composeBitmaps = (percentage, height, width) => {
     const alpha_offset_b = -255 * (1.0/(101 / (percentage+1)));
     const alpha_offset_a = -255 - alpha_offset_b;
 
     before.filters = [new createjs.ColorFilter(1, 1, 1, 1, 0, 0, 0, alpha_offset_b)];
+    // Don't cache on the first instance
     if (percentage !== 0)
       before.cache(0, 0, width, height);
 
     after.filters = [new createjs.ColorFilter(1, 1, 1, 1, 0, 0, 0, alpha_offset_a)];
-
     after.cache(0, 0, width, height);
-
-    stage.update()
   };
 
   return(<canvas ref={canvasRef}/>);
